498ba7c17a4a02d7362386928bc683284b5bc14a
camposcostero22 <camposcostero22@gmail.com>
2023-04-26 15:31:19 -0400

    Crear y separar controladores POSTS

--- server/routes/usuarios.js
+++ server/routes/usuarios.js
@@ -5,7 +5,7 @@ import { UserService } from "../controllers/users/index.js";
 export const routerUser = Router();
 //* Obtener todas las categorias - publico
 
-routerPost.get("/:userId", async (req, res = response) => {
+routerUser.get("/:userId", async (req, res = response) => {
   const { userId } = req.params;
 
   const posts = await prisma.post.findMany({
@@ -19,14 +19,14 @@ routerPost.get("/:userId", async (req, res = response) => {
   });
 });
 
-routerPost.post("/create", async (req, res = response) => {
+routerUser.post("/create", async (req, res = response) => {
   const { title, content } = req.body;
 
   const post = await prisma.user.create({});
   res.json({ post });
 });
 
-routerPost.get("/post/:id", async (req, res = response) => {
+routerUser.get("/post/:id", async (req, res = response) => {
   const { id } = req.params;
   const post = await prisma.post.findUnique({
     where: { id: +id },
@@ -34,7 +34,7 @@ routerPost.get("/post/:id", async (req, res = response) => {
   res.json({ post });
 });
 
-routerPost.put("/edit/:id", async (req, res) => {
+routerUser.put("/edit/:id", async (req, res) => {
   const { id } = req.params;
   const { title, content } = req.body;
   const post = await prisma.post.update({
@@ -44,15 +44,15 @@ routerPost.put("/edit/:id", async (req, res) => {
   res.json({ post });
 });
 
-routerPost.delete("/delete/:id", async (req, res) => {
+routerUser.delete("/delete/:id", async (req, res) => {
   const { id } = req.params;
   UserService.deleteUser(id);
 
   res.json({ post });
 });
 
-//TODO: modificar routerPOST crud usuarios
-//TODO: Crear y separar controladores Usuarios
+//* modificar routerPOST crud usuarios
+//! TODO: Crear y separar controladores Usuarios
 //TODO: Crear y separar controladores POSTS
 //TODO: Iniciar sesion
 //TODO: Crear DTO
